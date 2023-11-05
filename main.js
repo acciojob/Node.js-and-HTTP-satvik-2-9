6ee3fec28e4ad88024c7bb05fcd72d68dcbd05ea
thelastenvoy <satvikbatra@gmail.com>
2023-11-05 14:41:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
 
 const server = http.createServer((req, res) => {
-  // TODO: Implement this function
+  const parsedUrl = url.parse(req.url, true);
+  const method = req.method;
+  const path = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+
+  req.on('end', () => {
+    const parsedBody = querystring.parse(body);
+    
+    const responseObj = {
+      method,
+      path,
+      query,
+      body: parsedBody,
+    };
+
+    if (method !== 'POST') {
+      delete responseObj.body;
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(responseObj));
+  });
 });
 
-server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+/* we just simply export the server object, we donot start the server itself. */
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 module.exports = { server };
